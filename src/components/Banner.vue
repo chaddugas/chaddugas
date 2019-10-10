914b76f230c1457e34c77e1d437705e433a84f6b
Chad Dugas <chad.dug@gmail.com>
2019-10-10 04:23:45 -0600

    updates

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-section.sectionHead(style="{backgroundImage: $static.banner[0].node.image}")
-	.sectionhead-content
-		h1 $static.banner[0].node.main
-		h2 $static.banner[0].node.super
-		h2 $static.banner[0].node.sub
+	section.sectionHead
+		.secttionHead-parallax(:style="style")
+		.sectionHead-content
+			h1.sectionHead-main {{$static.banner.edges[0].node.main}}
+			h4.sectionHead-super {{$static.banner.edges[0].node.super}}
+			h4.sectionHead-sub {{$static.banner.edges[0].node.sub}}
 </template>
 
 <static-query>
@@ -22,7 +23,137 @@ query Banner {
 </static-query>
 
 <script>
+import Parallax from "@/mixins/Parallax";
 export default {
-	
-}
+  name: "Banner",
+  mixins: [Parallax],
+  computed: {
+		style() {
+			return {
+				...this.background,
+				...this.transform
+			}
+		},
+    background() {
+      return {
+        "background-image": `url(${this.$static.banner.edges[0].node.image})`
+      };
+    }
+  },
+  methods: {
+    log(e) {
+      console.log(e);
+    }
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.sectionHead {
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  height: 100vh;
+  padding: 110px 0 60px;
+  box-shadow: inset 0 -8px 8px -8px rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+	z-index: 1;
+  @media (min-width: 641px) {
+    align-items: center;
+    justify-content: center;
+    padding: 110px 0;
+  }
+}
+
+.secttionHead-parallax {
+  position: absolute;
+  top: -500px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: calc(100% + 500px);
+  background-position: 50% 50%;
+  background-size: cover;
+	will-change: transform;
+	z-index: -1;
+}
+
+.sectionHead-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 10px 20px;
+  @media (min-width: 641px) {
+    width: auto;
+  }
+}
+
+.sectionHead-main,
+.sectionHead-super,
+.sectionHead-sub {
+  position: relative;
+  padding: 0;
+  color: #fff;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.4);
+}
+
+.sectionHead-main {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  order: 2;
+  margin: 5px 0;
+  white-space: nowrap;
+  font-size: 30px;
+  &:before,
+  &:after {
+    position: relative;
+    top: 3px;
+    flex-shrink: 1;
+    width: 50px;
+    height: 3px;
+    content: "";
+    background: #fff;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
+    @media (min-width: 641px) {
+      width: 70px;
+      height: 5px;
+    }
+  }
+  &:before {
+    margin-right: 10px;
+  }
+  &:after {
+    margin-left: 10px;
+  }
+  @media (min-width: 641px) {
+    margin: 10px 0;
+    font-size: 40px;
+  }
+}
+
+.sectionHead-super,
+.sectionHead-sub {
+  margin: 0;
+  font-weight: 300;
+}
+
+.sectionHead-super {
+  order: 1;
+  font-size: 18px;
+  @media (min-width: 641px) {
+    font-size: 25px;
+  }
+}
+
+.sectionHead-sub {
+  order: 3;
+  font-size: 14px;
+  @media (min-width: 641px) {
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/mixins/Parallax.js
@@ -0,0 +1,54 @@
+export default {
+	data() {
+		return {
+			para_distance: 500,
+			para_start: window.innerHeight,
+			para_end: 0,
+			para_state: 'translateY(0)'
+		}
+	},
+	computed: {
+		transform() {
+			return { 'transform': this.para_state }
+		}
+	},
+	methods: {
+		para_update() {
+			this.para_start = window.innerHeight
+			this.para_end = this.$el.offsetHeight * -1
+		},
+		para_move() {
+			let top = this.$el.getBoundingClientRect().top
+			let move = this.between(top, this.para_start, this.para_end) * this.para_distance
+
+			move = move > this.para_distance ? this.para_distance : move < 0 ? 0 : move
+			
+			this.para_state = `translateY(${move}px)`
+		},
+		between(val, min, max, percent = false) {
+			let upper = val - min;
+			let lower = max - min;
+			let result = (upper / lower);
+		
+			if (percent) {
+				result = result * 100;
+			}
+		
+			result = parseFloat(result.toFixed(8));
+		
+			return result;
+		}
+	},
+	mounted() {
+		window.addEventListener('scroll', this.para_move)
+		window.addEventListener('resize', this.para_update)
+		window.addEventListener('orientationchange', this.para_update)
+		this.para_update()
+		this.para_move()
+	},
+	beforeDestroy() {
+		window.removeEventListener('scroll', this.para_move)
+		window.removeEventListener('resize', this.para_update)
+		window.removeEventListener('orientationchange', this.para_update)
+	}
+}
