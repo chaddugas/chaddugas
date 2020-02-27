cbe535c9e6e34c442a8cbc548aa26f4cba4e0de9
Chad Dugas <chad.dug@gmail.com>
2020-02-27 13:18:52 -0700

    updates

--- src/components/HeroLogo.vue
+++ src/components/HeroLogo.vue
@@ -119,12 +119,14 @@ export default {
   align-items: flex-start;
 	margin: 0 -0.5rem;
 	opacity: 0.3;
-  svg {
+	svg,
+	i {
     margin: 0 0.5rem;
   }
   @media (min-width: $sm) {
     margin: 0 -0.75rem;
-    svg {
+		svg,
+		i {
       margin: 0 0.75rem;
     }
   }
